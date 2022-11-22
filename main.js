d734b4c84cd27ebdc041719a26f810f026c52ddf
Ingrid E. Torres <ingrid.escabal@gmail.com>
2022-11-22 09:45:04 +0800

    modernized the looks

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
     let myHeaders = new Headers();
     myHeaders.append("apikey", "uM1qXhFzy1o5R7w2hgVfrHMJyZWNl9bz");
@@ -11,40 +11,40 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 
     fetch(`https://api.apilayer.com/exchangerates_data/symbols?symbols`, requestOptions)
-  .then(response => response.json())
-  .then(data => {
-    let {symbols} =  data;
-    symbolkeys = Object.keys(symbols);
-    lengthKeys = Object.keys(symbols).length;
+        .then(response => response.json())
+        .then(data => {
+            let { symbols } = data;
+            symbolkeys = Object.keys(symbols);
+            lengthKeys = Object.keys(symbols).length;
 
-    for (let i=1; i<=lengthKeys; i++){
-        let value1 = symbolkeys[i];
-    document.querySelector("#from").innerHTML += "<option value="+value1+">"+value1+"</option>";
-    };
-    
-    for (let i=1; i<=lengthKeys; i++){
-        let value1 = symbolkeys[i];
-        console.log(value1);
-        console.log("<option value="+value1+">value1</option>");
-    document.querySelector("#to").innerHTML += "<option value="+value1+">"+value1+"</option>";
-    };
+            for (let i = 1; i <= lengthKeys; i++) {
+                let value1 = symbolkeys[i];
+                document.querySelector("#from").innerHTML += "<option value=" + value1 + ">" + value1 + "</option>";
+            };
 
-})
-  .catch(error => console.log('error', error));
-// Sample data from symbols
-// {
-//     "success": true,
-//     "symbols": {
-//       "AED": "United Arab Emirates Dirham",
-//       "AFN": "Afghan Afghani",
-//       "ALL": "Albanian Lek",
-//       "AMD": "Armenian Dram"
-//     }
-//   }
+            for (let i = 1; i <= lengthKeys; i++) {
+                let value1 = symbolkeys[i];
+                console.log(value1);
+                console.log("<option value=" + value1 + ">value1</option>");
+                document.querySelector("#to").innerHTML += "<option value=" + value1 + ">" + value1 + "</option>";
+            };
 
-    document.querySelector("#currency-converter").addEventListener("submit", (event)=>{
+        })
+        .catch(error => console.log('error', error));
+    // Sample data from symbols
+    // {
+    //     "success": true,
+    //     "symbols": {
+    //       "AED": "United Arab Emirates Dirham",
+    //       "AFN": "Afghan Afghani",
+    //       "ALL": "Albanian Lek",
+    //       "AMD": "Armenian Dram"
+    //     }
+    //   }
+
+    document.querySelector("#currency-converter").addEventListener("submit", (event) => {
         event.preventDefault();  //prevents default behavior; does not show in URL the values.
-        const {target: {from, to, amount}} = event;
+        const { target: { from, to, amount } } = event;
 
         //response: 
         // {
@@ -61,16 +61,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
         //     "date": "2022-10-28",
         //     "result": 638.3685
         // }
-        
-         fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
-        .then(response=>response.json())
-        .then(data =>{
-            console.log(data);
-            let {info, date, query:{to}, result} =  data;   //available from server response: success, query, info, date
-            document.querySelector(".result").innerHTML = `<b id="from-value">${from.value.toUpperCase()} ${amount.valueAsNumber.toFixed(2)}  =  </b><b id="final-result">${to} ${result.toFixed(2)}</b>`;
-        })
-        .catch(error => console.log('error', error));
+
+        fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let { info, date, query: { to }, result } = data;   //available from server response: success, query, info, date
+                document.querySelector(".result").innerHTML = `<b id="from-value">${from.value.toUpperCase()} ${amount.valueAsNumber.toFixed(2)}  =  </b><b id="final-result">${to} ${result.toFixed(2)}</b>`;
+            })
+            .catch(error => console.log('error', error));
     })
- 
+
 
 });
